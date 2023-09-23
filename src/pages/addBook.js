6acc230fd5c9f9d21bbbc2a450f7ddff6aa7b737
Youssef_Hatem <69750206+JOJOREO@users.noreply.github.com>
2023-09-24 02:35:22 +0300

    add book ui done

--- src/pages/addBook.js
+++ src/pages/addBook.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import SideBar from "../components/sideBar";
 import Navbar from "../components/Navbar";
+import bookmarkIcon from "../images/352230_bookmark_icon.png";
 
 import { toggleSideBarFunction } from "../store/actions";
 import { bookSetter } from "../store/actions";
@@ -23,8 +24,22 @@ const AddBook = (props) => {
   const [bookReleaseDate, setBookReleaseDate] = useState("");
   const [bookCoverImage, setBookCoverImage] = useState("");
   const [bookBrief, setBookBrief] = useState("");
+  const [inputType, setInputType] = useState("text");
 
-  const navigate = useNavigate();
+  const [uploadBookCover, setUploadBookCover] = useState();
+  const dateRef = useRef();
+  const handleChange = () => {
+    // console.log(dateRef);
+    // console.log(dateRef.type);
+    // dateRef.type = "date";
+    setInputType("date");
+  };
+  const handleBlur = () => {
+    // dateRef.type = "text";
+    // console.log(dateRef);
+    // console.log(dateRef.type);
+    setInputType("text");
+  };
 
   // useEffect(() => {
   //   if (!store.getState().user.data?.email) navigate("/");
@@ -116,9 +131,7 @@ const AddBook = (props) => {
                   </span>
 
                   <p
-                    className={` "missing-field-warning"${
-                      bookTitle ? "invisibleSpan" : ""
-                    }`}
+                    className={`${bookTitle ? "invisibleSpan" : ""}`}
                     // className="missing-field-warning"
                     // style={{ color: "red" }}
                   >
@@ -153,14 +166,14 @@ const AddBook = (props) => {
                   >
                     *
                   </span>
-                  {!bookTitle && (
-                    <p
-                      className="missing-field-warning"
-                      // style={{ color: "#dc3545" }}
-                    >
-                      Book Author is required.
-                    </p>
-                  )}
+
+                  <p
+                    className={`${bookAuthor ? "invisibleSpan" : ""}`}
+                    // className="missing-field-warning"
+                    // style={{ color: "#dc3545" }}
+                  >
+                    Book Author is required.
+                  </p>
                 </div>
                 <div
                   style={{
@@ -198,14 +211,14 @@ const AddBook = (props) => {
                   >
                     *
                   </span>
-                  {!categories && (
-                    <p
-                      className="missing-field-warning"
-                      // style={{ color: "#dc3545" }}
-                    >
-                      Categories is required.
-                    </p>
-                  )}
+
+                  <p
+                    className={`${categories ? "invisibleSpan" : ""}`}
+                    // className="missing-field-warning"
+                    // style={{ color: "#dc3545" }}
+                  >
+                    Categories is required.
+                  </p>
                 </div>
                 <div
                   style={{
@@ -235,14 +248,14 @@ const AddBook = (props) => {
                   >
                     *
                   </span>
-                  {!bookPrice && (
-                    <p
-                      className="missing-field-warning"
-                      // style={{ color: "#dc3545" }}
-                    >
-                      Book Price is required.
-                    </p>
-                  )}
+
+                  <p
+                    className={`${bookPrice ? "invisibleSpan" : ""}`}
+                    // className="missing-field-warning"
+                    // style={{ color: "#dc3545" }}
+                  >
+                    Book Price is required.
+                  </p>
                 </div>
                 <div
                   style={{
@@ -274,14 +287,14 @@ const AddBook = (props) => {
                   >
                     *
                   </span>
-                  {!bookVersion && (
-                    <p
-                      className="missing-field-warning"
-                      // style={{ color: "#dc3545" }}
-                    >
-                      Book Version is required.
-                    </p>
-                  )}
+
+                  <p
+                    className={`${bookVersion ? "invisibleSpan" : ""}`}
+                    // className="missing-field-warning"
+                    // style={{ color: "#dc3545" }}
+                  >
+                    Book Version is required.
+                  </p>
                 </div>
                 <select
                   className="form-select form-control"
@@ -330,6 +343,7 @@ const AddBook = (props) => {
                   style={{
                     margin: "0",
                     padding: "0",
+                    // height: "15%",
                     // display: "flex",
                     // flexDirection: "row",
                   }}
@@ -359,51 +373,216 @@ const AddBook = (props) => {
                   >
                     *
                   </span>
-
-                  {!bookISBN && (
-                    <p
-                      className="missing-field-warning"
-                      // style={{ color: "#dc3545" }}
-                    >
-                      Book ISBN is required.
-                    </p>
-                  )}
+                  <p
+                    style={
+                      {
+                        // backgroundColor: "red",
+                        // width: "100%",
+                        // height: "100%",
+                      }
+                    }
+                    // className="missing-field-warning"
+                    className={`${bookISBN ? "invisibleSpan" : ""}`}
+                  >
+                    Book ISBN is required.
+                  </p>
                 </div>
                 <input
+                  // onFocus={handleChange}
                   style={{ color: "grey" }}
-                  type="date"
-                  // type="text"
-                  // onFocus="(this.type='date')"
-                  // onBlur="(this.type='text')"
+                  type={inputType}
+                  onClick={handleChange}
+                  onBlur={handleBlur}
                   className="form-control"
                   placeholder="Book Release Date"
-                  // required
                   value={bookReleaseDate}
                   onChange={(e) => {
                     setBookReleaseDate(e.target.value.toString());
                     console.log(bookReleaseDate);
                   }}
                 ></input>
-                {/* {!bookTitle && (
-                  <p
-                    className="missing-field-warning"
-                    // style={{ color: "#dc3545" }}
-                  >
-                    Book Author is required.
-                  </p>
-                )} */}
               </div>
-              <div
-                className="right"
-                // style={{
-                //   border: "1px solid green",
-                //   width: "100%",
-                //   height: "100%",
-                // }}
-              >
-                <button className="btn btn-primary" type="submit">
+              <div className="right">
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <img
+                    // src={book?.volumeInfo?.imageLinks?.thumbnail}
+                    // alt={store.getState().book.book?.volumeInfo?.title}
+                    style={{
+                      width: "120px",
+                      height: "180px",
+                      borderRadius: "10px",
+                      objectFit: "cover",
+                      boxShadow: " 2px 4px white, 4px 6px gray",
+                      // marginBottom: "15px",
+                      // paddingTop: "10px",
+                    }}
+                  ></img>
+                  <img
+                    src={bookmarkIcon}
+                    style={{
+                      width: "18px",
+                      // height: "13px",
+                      margin: "0",
+                      padding: "0",
+                      marginLeft: "20px",
+                    }}
+                  ></img>
+                </div>
+                <p style={{ color: "gray", fontSize: "10px" }}>
+                  Best dimensions for book cover image are 128*200
+                </p>
+                <div>
+                  <button
+                    type="button"
+                    className="btn-outline-primary btn upload-btn"
+                    style={{
+                      background: "none",
+                      border: "2px solid #E03FD8",
+                      borderRadius: "12px",
+                      fontSize: "12px",
+                      marginBottom: "10px",
+                      color: "#E03FD8",
+                      borderRadius: "5px",
+                      fontWeight: "bold",
+                      borderRadius: "12px",
+                      padding: "10px 20px",
+                      // width: "10vw",
+                    }}
+                  >
+                    Upload Book Cover
+                    <input
+                      type="image"
+                      placeholder="Upload Book Cover"
+                      required
+                      onSubmit={(e) => {
+                        setBookCoverImage(e.target.value);
+                      }}
+                      style={{
+                        visibility: "hidden",
+                        display: "none",
+                      }}
+                    />
+                  </button>
+                  <span
+                    className={`${uploadBookCover ? "invisibleSpan" : ""}`}
+                    style={{
+                      display: "block",
+                      color: "#E03FD8",
+                      position: "relative",
+                      top: "-42px",
+                      right: "-131px",
+                    }}
+                  >
+                    *
+                  </span>
+                </div>
+                <div
+                // style={{ display: "flex", flexDirection: "column" }}
+                >
+                  <textarea
+                    className="form-control"
+                    name=""
+                    id=""
+                    cols="86"
+                    style={{
+                      resize: "none",
+                      // width: "100%",
+                      // padding: "15px",
+                      borderRadius: "5px",
+                      // paddingTop: "10px",
+                      // paddingLeft: "10px",
+                      backgroundColor: "#eff1fd",
+                    }}
+                    rows="5"
+                    value={bookBrief}
+                    onChange={(e) => {
+                      setBookBrief(e.target.value);
+                    }}
+                    placeholder="Book Brief"
+                  ></textarea>
+                  <span
+                    className={`${bookBrief ? "invisibleSpan" : ""}`}
+                    style={{
+                      display: "block",
+                      color: "red",
+                      position: "relative",
+                      top: "-110px",
+                      right: "-85px",
+                    }}
+                  >
+                    *
+                  </span>
+                  <p
+                    style={{
+                      color: "gray",
+                      fontSize: "10px",
+                      float: "right",
+                      display: "inline-block",
+                    }}
+                  >{`${bookBrief.length}/800`}</p>
+                </div>
+
+                {/* <button className="btn btn-primary" type="submit">
                   Save
-                </button>
+                </button> */}
+
+                <div>
+                  <div
+                    className="book-buttons"
+                    style={{
+                      // border: "1px solid purple",
+                      // display: "flex",
+                      // flexDirection: "row",
+                      float: "right",
+                      display: "grid",
+                      gridTemplateColumns: "auto auto",
+                      gridTemplateRows: "auto",
+                      gridGap: "10px",
+                      height: "20%",
+                      position: "absolute",
+                      right: "40px",
+                      // right: "-226px",
+                    }}
+                  >
+                    <div>
+                      <button
+                        style={{
+                          borderRadius: "5px",
+                          color: "black",
+                          fontWeight: "bold",
+                          borderRadius: "12px",
+                          padding: "10px",
+                          width: "5vw",
+                          // backgroundColor: "#b0011c",
+                        }}
+                        className="btn "
+                        onClick={() => {
+                          // setToggleToast(!toggleToast);
+                          // console.log(toggleToast);
+                        }}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        type="submit"
+                        style={{
+                          backgroundColor: "#0c4dcc",
+                          borderRadius: "5px",
+                          fontWeight: "bold",
+                          borderRadius: "12px",
+                          padding: "10px",
+                          width: "4vw",
+                          // width: "fit-contents",
+                        }}
+                        className="btn btn-primary"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </form>
